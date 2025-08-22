406ee28f95dd621479d1a3d4f8a6bbb4a068894b
Cat <prasertsakul.p@gmail.com>
2025-08-22 13:37:38 +0700

    Update

--- app_web/urls.py
+++ app_web/urls.py
@@ -4,4 +4,10 @@ from . import views
 urlpatterns = [
     # HTTPRESPONSE
     path('', views.home, name='home'),
+    path('', views.home, name='student'),
+    path('', views.home, name='gacha'),
+    path('', views.home, name='login'),
+    path('', views.home, name='logout'),
+    path('', views.home, name='dashboard'),
+    path('', views.home, name='register'),
 ]
